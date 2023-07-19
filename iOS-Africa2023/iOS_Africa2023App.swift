456899f5aaa5bb7669a39d899174eda820471ce3
Gan Tu <github@tugan.dev>
2023-07-18 22:14:32 -0400

    Initial Commit

---
+++ iOS-Africa2023/iOS_Africa2023App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Africa2023App.swift
+//  iOS-Africa2023
+//
+//  Created by Gan Tu on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Africa2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
