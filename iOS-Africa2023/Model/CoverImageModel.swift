276aa1aa951da039a94805744239bd4e7d78f3c7
Gan Tu <github@tugan.dev>
2023-07-18 23:10:51 -0400

    feat: add browse cover image carousel loaded from json files

---
+++ iOS-Africa2023/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  iOS-Africa2023
+//
+//  Created by Gan Tu on 7/18/23.
+//
+
+import Foundation
+
+struct CoverImage: Identifiable, Codable {
+    let id: Int
+    let name: String
+}
---
+++ iOS-Africa2023/View/CoverImageView.swift
@@ -0,0 +1,29 @@
+//
+//  CoverImageView.swift
+//  iOS-Africa2023
+//
+//  Created by Gan Tu on 7/18/23.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    let coverImages: [CoverImage] = Bundle.main.decode("covers.json")
+    
+    // MARK: - BODY
+    var body: some View {
+        TabView {
+            ForEach(coverImages) { item in
+                Image(item.name)
+                    .resizable()
+                    .scaledToFill()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+#Preview {
+    CoverImageView()
+}
