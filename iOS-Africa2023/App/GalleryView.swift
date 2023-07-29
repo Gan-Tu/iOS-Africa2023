c4594d0d7032563e5eba0149f5d770b29f2b210a
Gan Tu <github@tugan.dev>
2023-07-29 16:34:24 -0400

    feat: add slider based gallery grid

--- iOS-Africa2023/App/GalleryView.swift
+++ iOS-Africa2023/App/GalleryView.swift
@@ -8,9 +8,67 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    // Simple grid definitions
+    // let gridLayout: [GridItem] = [
+    //     GridItem(.flexible()),
+    //     GridItem(.flexible()),
+    //     GridItem(.flexible())
+    // ]
+    
+    // Efficient grid definitions
+    // let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    // Dynamic grid definitions
+    @State private var gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                Slider(value: $gridColumn, in: 2...5, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: {value in
+                        gridSwitch()
+                    })
+                
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    }
+                }
+                .onAppear(perform: {
+                    withAnimation(.easeIn) {
+                        gridSwitch()
+                    }
+                })
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }
         .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
         .background(MotionAnimationView())
